be5a3383adcacd2b5677c1377cd9e5793fe0a788
HedvigS <hedvig.skirgard@gmail.com>
2023-08-17 22:41:40 +0200

    Update 04_demo.R

--- 04_demo.R
+++ 04_demo.R
@@ -5,7 +5,9 @@ plot_eez_map_kalyan(elevation_shading = F, south = "down",
                     colour_water_EEZ = "azure1",
                     colour_water = "white", 
                     colour_land = "aliceblue", 
-                    colour_outline_eez = "lightgrey"
+                    colour_outline_eez = "lightgrey", 
+                    xlim = c(20, 280),
+                    ylim = c(-50, 50)
 )
 
 x <- dev.off()
