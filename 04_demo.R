2f9d992eaf60f3538304337fda84db00fee87d63
HedvigS <hedvig.skirgard@gmail.com>
2023-08-17 22:39:20 +0200

    Update 04_demo.R

--- 04_demo.R
+++ 04_demo.R
@@ -1,8 +1,11 @@
 source("03_funs.R")
 
-
+png(filename = "test.png", width = 20, height = 15, units = "cm", res = 400)
 plot_eez_map_kalyan(elevation_shading = F, south = "down", 
                     colour_water_EEZ = "azure1",
                     colour_water = "white", 
-                    colour_land = "aliceblue"
+                    colour_land = "aliceblue", 
+                    colour_outline_eez = "lightgrey"
 )
+
+x <- dev.off()
