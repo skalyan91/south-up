86f286435365cb72982ca120a1e45e87d8425016
HedvigS <hedvig.skirgard@gmail.com>
2023-08-17 22:27:54 +0200

    Create 04_demo.R

---
+++ 04_demo.R
@@ -0,0 +1,8 @@
+source("03_funs.R")
+
+
+plot_eez_map_kalyan(elevation_shading = F, south = "down", 
+                    colour_water_EEZ = "azure1",
+                    colour_water = "white", 
+                    colour_land = "aliceblue"
+)
